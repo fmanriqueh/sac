cf0f932236e422d51248515e702ded0ecd2c2789
Sistemas2 <sistemas2@iMac-de-Sistemas2.local>
2020-10-23 14:55:15 -0500

    Login created

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,10 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import AuthModule from './auth.module'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    AuthModule
   }
 })
