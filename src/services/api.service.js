3f0f1b3aabbbcf0f35d84f9f6c236032cf818955
Sistemas2 <sistemas2@iMac-de-Sistemas2.local>
2020-10-23 15:09:25 -0500

    bad

---
+++ src/services/api.service.js
@@ -0,0 +1,3 @@
+import Vue from 'Vue';
+import axios from 'axios';
+
---
+++ src/views/Login.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <div class="log-in">
+            <form >
+
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import { stateMapping } from '@store'
+
+export default {
+    data(){
+        return {
+            user: {
+                
+            }
+        }
+    },
+    computed: {
+        ...stateMapping(['currentUser'])
+    },
+    methods: {
+        onSubmit(){
+
+        }
+    }
+
+}
+</script>
